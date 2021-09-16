1ef9c93bc7cf765d18aed4b24cdf9621629d8a93
ming995 <hymghost@gmail.com>
2021-09-16 15:11:57 +0800

    chore: update

--- .storybook/SelfTheme.js
+++
@@ -1,9 +0,0 @@
-// 自定义主题
-import { create } from "@storybook/theming";
-
-export default create({
-  base: "light",
-  brandTitle: "My custom storybook",
-  brandUrl: "https://example.com",
-  brandImage: "https://place-hold.it/350x150",
-});
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,37 +1,10 @@
-const path = require("path");
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: "react-docgen-typescript",
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-    },
-  },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../"),
-    });
-
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve("babel-loader"),
-      options: {
-        presets: [["react-app", { flow: false, typescript: true }]],
-      },
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-
-    return config;
-  },
-};
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
